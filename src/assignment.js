242d9618020d49b9395fc22c7a96e835d210da56
Nnamani  Ugochukwu Frederick <65407190+ddarkCode@users.noreply.github.com>
2022-07-10 15:29:41 +0000

    Complete challenge-1 and challenge-2 of the Javascript Fluency Assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce((prev, cur) => {
+      return prev + cur
+    }, 0);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.filter(num => num % 2 === 0).length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
